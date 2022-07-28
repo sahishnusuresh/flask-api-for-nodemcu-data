57c66a23ab22566dd44fdc90794fc632f9480c57
sahishnusuresh <sahishnu.suresh@gmail.com>
2022-07-28 13:37:29 +0530

    initial

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from flask import Flask,request
+ 
+app = Flask(__name__)
+ 
+@app.route("/",methods=['GET'])
+def home_view():
+        return "hello"
+@app.route('/sensor/data',methods='POST')
+def posting():
+    content=request.get_json()
+    print(content)
+    return content
