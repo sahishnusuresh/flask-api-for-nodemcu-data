03d9c86fa5d3d9a8122fd4cab252b1e5313b0ce0
sahishnusuresh <sahishnu.suresh@gmail.com>
2022-07-28 22:01:10 +0530

    checking type

--- app/main.py
+++ app/main.py
@@ -1,12 +1,24 @@
 from flask import Flask,request
- 
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 #google_cloud_stuff
 PASSWORD ="sahi666"
-PUBLIC_IP_ADDRESS ="34.173.189.62"
-DBNAME ="database name"
-PROJECT_ID ="gcp project id"
-INSTANCE_NAME ="instance name"
+PUBLIC_IP_ADDRESS ="35.222.14.19"
+DBNAME ="sensor_data"
+PROJECT_ID ="AQ-CLOUD"
+INSTANCE_NAME ="flask-esp"
+# app.config["SECRET_KEY"] = "yoursecretkey"
+# app.config["SQLALCHEMY_DATABASE_URI"]= f"mysql + mysqldb://root:{PASSWORD}@{PUBLIC_IP_ADDRESS}/{DBNAME}?unix_socket =/cloudsql/{PROJECT_ID}:{INSTANCE_NAME}"
+# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= True
+ 
+db = SQLAlchemy(app)
+ 
+# # User ORM for SQLAlchemy
+# class Users(db.Model):
+#     id = db.Column(db.Integer, primary_key = True, nullable = False)
+#     name = db.Column(db.String(50), nullable = False)
+#     email = db.Column(db.String(50), nullable = False, unique = True)
  
 @app.route("/",methods=['GET'])
 def home_view():
