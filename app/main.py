98c61549a295eb989fba0cb2e238fa1c18c4dcd7
sahishnusuresh <sahishnu.suresh@gmail.com>
2022-07-28 15:56:25 +0530

    added get to data

--- app/main.py
+++ app/main.py
@@ -1,11 +1,17 @@
 from flask import Flask,request
  
 app = Flask(__name__)
+#google_cloud_stuff
+PASSWORD ="sahi666"
+PUBLIC_IP_ADDRESS ="34.173.189.62"
+DBNAME ="database name"
+PROJECT_ID ="gcp project id"
+INSTANCE_NAME ="instance name"
  
 @app.route("/",methods=['GET'])
 def home_view():
         return "hello"
-@app.route('/sensor/data',methods=['POST'])
+@app.route('/sensor/data',methods=['GET','POST'])
 def posting():
     content=request.get_json()
     print(content)
