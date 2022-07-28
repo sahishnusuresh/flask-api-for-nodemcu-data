f4a0e2983950c6353245f5f89171260a0b27807d
sahishnusuresh <sahishnu.suresh@gmail.com>
2022-07-28 14:49:27 +0530

    app post method file changed

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/",methods=['GET'])
 def home_view():
         return "hello"
-@app.route('/sensor/data',methods='POST')
+@app.route('/sensor/data',methods=['POST'])
 def posting():
     content=request.get_json()
     print(content)
