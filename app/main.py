aa25b10a1218f65a77b558fc983f596df194b5dc
sahishnusuresh <sahishnu.suresh@gmail.com>
2022-07-29 13:41:57 +0530

    connected database

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify,request
+from flask import Flask, jsonify,request,make_response
 from flask_sqlalchemy import SQLAlchemy
 import json
 
@@ -9,18 +9,18 @@ PUBLIC_IP_ADDRESS ="35.222.14.19"
 DBNAME ="sensor_data"
 PROJECT_ID ="AQ-CLOUD"
 INSTANCE_NAME ="flask-esp"
-# app.config["SECRET_KEY"] = "yoursecretkey"
-# app.config["SQLALCHEMY_DATABASE_URI"]= f"mysql + mysqldb://root:{PASSWORD}@{PUBLIC_IP_ADDRESS}/{DBNAME}?unix_socket =/cloudsql/{PROJECT_ID}:{INSTANCE_NAME}"
-# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= True
+app.config["SECRET_KEY"] = "yoursecretkey"
+app.config["SQLALCHEMY_DATABASE_URI"]= f"mysql + mysqldb://root:{PASSWORD}@{PUBLIC_IP_ADDRESS}/{DBNAME}?unix_socket =/cloudsql/{PROJECT_ID}:{INSTANCE_NAME}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= True
  
 db = SQLAlchemy(app)
  
 
-# class Sensors(db.Model):
-#     sensor_id=db.Column(db.String(50), nullable=False)
-#     pm10=db.Column(db.Integer, nullable=False)
-#     pm75=db.Column(db.Integer, nullable=False)
-#     pm25=db.Column(db.Integer,nullable=False)
+class Sensors(db.Model):
+    sensor_id=db.Column(db.String(50), nullable=False)
+    aq1_pm10=db.Column(db.Integer, nullable=False)
+    aq1_pm75=db.Column(db.Integer, nullable=False)
+    aq1_pm25=db.Column(db.Integer,nullable=False)
  
 @app.route("/",methods=['GET'])
 def home_view():
@@ -36,6 +36,51 @@ def posting():
     # content=request.get_json()
     # print(content)
     # print(validateJSON(content))
+    
     content=request.get_json()
     print(content['aq1']['pm10'])
-    return 'json posted'
+    
+    aq1_pm10=content['aq1']['pm10']
+    aq1_pm75=content['aq1']['pm75']
+    aq1_pm25=content['aq1']['pm25']
+    try:
+        sensor=Sensors(
+        sensor_id="aq1",
+        aq1_pm10=aq1_pm10,
+        aq1_pm75=aq1_pm75,
+        aq1_pm25=aq1_pm25
+        )
+        db.session.add(sensor)
+        db.session.commit()
+        responseObject = {
+                'status' : 'success',
+                'message': 'Successfully registered.'
+            }
+ 
+        return make_response(responseObject, 200)
+    except:
+            responseObject = {
+                'status' : 'fail',
+                'message': 'Some error occured !!'
+            }
+ 
+            return make_response(responseObject, 400)
+@app.route('/view',methods=['GET'])
+def show():
+    response=list()
+    sensors=Sensors.query.all()
+    for sensor in sensors:
+        response.append({
+            "sensor_id":sensor.sensor_id,
+            "aq1_pm10":sensor.aq1_pm10,
+            "aq1_pm75":sensor.aq1_pm75,
+            "aq1_pm25":sensor.aq1_pm25
+        })
+        return make_response({
+        'status' : 'success',
+        'message': response
+        }, 200)
+
+
+
+
